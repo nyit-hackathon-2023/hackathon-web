d7f8109407dfc775258e1ad7c1bd5d28658b0f65
Subrat Kumar Singh <subratsinghsks_cse16@its.edu.in>
2023-04-08 12:47:41 -0400

    added routes for the initial structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
 import TestChatPage from "./TestChat";
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <TestChatPage/>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/chat' element={<TestChatPage/>} ></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
